15b69b2927a6203d68c4e5b74e9b64cc243d9c23
Lei <slei12@corp.bloomberg.com>
2016-06-14 16:58:32 -0400

    Updates

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.listen(8080, function() {
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(8080, function() {
     console.log('listening on port 8080');
 });
 
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
